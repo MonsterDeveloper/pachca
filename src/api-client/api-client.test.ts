47b0e8262e00c0f07a46d21f5a516ae55cc93568
Andrei Bobkov <slider7259@gmail.com>
2023-10-16 23:47:41 +0200

    feat(api-client.ts): introduce API client

---
+++ src/api-client/api-client.test.ts
@@ -0,0 +1,154 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { ApiClient } from "./api-client";
+import { PACHCA_BASE_URL, EXAMPLE_BASE_URL } from "mocks/constants";
+import { PachcaError } from "@/errors";
+
+describe("ApiClient", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  describe("request", () => {
+    it("should send a request with default Pachca base URL", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({ accessToken: "test" });
+
+      await client.request("/");
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        PACHCA_BASE_URL + "/",
+        expect.objectContaining({
+          headers: expect.objectContaining({
+            Authorization: "Bearer test",
+            "Content-Type": "application/json; charset=utf-8",
+          }),
+        }),
+      );
+    });
+
+    it("should send a request with custom base URL", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({
+        accessToken: "test",
+        baseUrl: EXAMPLE_BASE_URL,
+      });
+
+      await client.request("/");
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        EXAMPLE_BASE_URL + "/",
+        expect.any(Object),
+      );
+    });
+
+    it("should send a request with absolute URL", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({
+        accessToken: "test",
+      });
+
+      await client.request(EXAMPLE_BASE_URL);
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        EXAMPLE_BASE_URL,
+        expect.any(Object),
+      );
+    });
+
+    it("should send a request with search parameters", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({ accessToken: "test" });
+
+      await client.request("/", {
+        searchParameters: new URLSearchParams({ q: "test" }),
+      });
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        PACHCA_BASE_URL + "/?q=test",
+        expect.any(Object),
+      );
+    });
+
+    it("should send a request with a JSON body", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({ accessToken: "test" });
+
+      await client.request("/", {
+        method: "POST",
+        body: { name: "John Doe" },
+      });
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        PACHCA_BASE_URL + "/",
+        expect.objectContaining({
+          method: "POST",
+          body: JSON.stringify({ name: "John Doe" }),
+        }),
+      );
+    });
+
+    it("should send a request with a FormData body", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({ accessToken: "test" });
+      const formData = new FormData();
+      formData.append("file", new Blob(["test"], { type: "text/plain" }));
+
+      await client.request("/", {
+        method: "POST",
+        body: formData,
+      });
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        PACHCA_BASE_URL + "/",
+        expect.objectContaining({
+          method: "POST",
+          body: formData,
+          headers: expect.objectContaining({
+            "Content-Type": "multipart/form-data",
+          }),
+        }),
+      );
+    });
+
+    it("should throw an error if a response is not OK", async () => {
+      const client = new ApiClient({
+        accessToken: "",
+        baseUrl: "https://example.com",
+      });
+
+      await expect(client.request("/error")).rejects.toThrow(PachcaError);
+    });
+  });
+
+  describe("get", () => {
+    it("should send a GET request", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({ accessToken: "test" });
+
+      await client.get("/");
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          method: "GET",
+        }),
+      );
+    });
+  });
+
+  describe("post", () => {
+    it("should send a POST request", async () => {
+      const fetchSpy = vi.spyOn(globalThis, "fetch");
+      const client = new ApiClient({ accessToken: "" });
+
+      await client.post("/");
+
+      expect(fetchSpy).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          method: "POST",
+        }),
+      );
+    });
+  });
+});
---
+++ src/api-client/api-client.ts
@@ -0,0 +1,101 @@
+import { version } from "package.json";
+import { handleError } from "./handle-error";
+
+/**
+ * Опции создания API клиента
+ */
+export interface ApiClientOptions {
+  /** Базовый URL API
+   * @default https://api.pachca.com/api/shared/v1
+   */
+  baseUrl?: string;
+
+  /** Токен доступа
+   * @see https://crm.pachca.com/dev/getting-started/requests-and-responses/
+   */
+  accessToken: string;
+
+  /** Заголовок User-Agent в запросах
+   * @default pachca/${version} (+https://github.com/MonsterDeveloper/pachca)
+   */
+  userAgent?: string;
+
+  /** ID пользователя/бота, которому принадлежит токен */
+  userId?: number;
+}
+
+/** Опции запроса. Поддерживаются все стандартные параметры для fetch, а также query параметры и тело запроса — JSON или FormData */
+type RequestOptions = Omit<RequestInit, "body"> & {
+  body?: object | FormData;
+  searchParameters?: URLSearchParams;
+};
+type RequestOptionsWithoutMethod = Omit<RequestOptions, "method">;
+
+/**
+ * Клиент для работы с API
+ */
+export class ApiClient {
+  private baseUrl: string;
+  private userAgent: string;
+  private accessToken: string;
+  private userId?: number;
+
+  constructor(options: ApiClientOptions) {
+    this.baseUrl = options.baseUrl ?? "https://api.pachca.com/api/shared/v1";
+    this.userAgent =
+      options.userAgent ??
+      `pachca/${version} (+https://github.com/MonsterDeveloper/pachca)`;
+    this.accessToken = options.accessToken;
+    this.userId = options.userId;
+  }
+
+  /**
+   * Выполняет запрос к API
+   * @param endpoint Путь к методу API (абсолютный или относительный)
+   * @param options Опции запроса
+   */
+  async request(
+    endpoint: string,
+    { searchParameters, ...options }: RequestOptions = {},
+  ) {
+    const url = endpoint.startsWith("http")
+      ? endpoint
+      : this.baseUrl + endpoint;
+
+    const response = await fetch(
+      url + (searchParameters ? `?${searchParameters}` : ""),
+      {
+        ...options,
+        body:
+          options?.body instanceof FormData
+            ? options.body
+            : options?.body
+            ? JSON.stringify(options.body)
+            : undefined,
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+          "User-Agent": this.userAgent,
+          "Content-Type":
+            options?.body instanceof FormData
+              ? "multipart/form-data"
+              : "application/json; charset=utf-8",
+          ...options.headers,
+        },
+      },
+    );
+
+    if (!response.ok) await handleError(response);
+
+    return response;
+  }
+
+  /** Выполняет GET запрос */
+  async get(url: string, options: RequestOptionsWithoutMethod = {}) {
+    return this.request(url, { ...options, method: "GET" });
+  }
+
+  /** Выполняет POST запрос */
+  async post(url: string, options: RequestOptionsWithoutMethod = {}) {
+    return this.request(url, { ...options, method: "POST" });
+  }
+}
