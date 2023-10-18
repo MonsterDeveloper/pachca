ee125871c220b16f4485771a8c5cbf09b671fbbb
Andrei Bobkov <slider7259@gmail.com>
2023-10-18 16:48:37 +0200

    feat(handle-error.ts): add support for XML errors

--- src/api-client/handle-error.test.ts
+++ src/api-client/handle-error.test.ts
@@ -10,20 +10,20 @@ describe("handleError", () => {
     );
   });
 
-  it("should throw a PachcaError if the response Content-Type is not application/json", async () => {
+  it("should throw a PachcaError if the response Content-Type is not application/json and text/xml", async () => {
     const response = new Response(undefined, {
       status: 400,
       headers: { "Content-Type": "text/plain" },
     });
     await expect(handleError(response)).rejects.toThrow(
       new PachcaError(
-        "Заголовок Content-Type в ответе не application/json",
+        "Заголовок Content-Type text/plain в ответе не поддерживается",
         response,
       ),
     );
   });
 
-  it("should throw a PachcaError when the response body is empty but Content-Type is application/json", async () => {
+  it("should throw a PachcaError when the response body is empty", async () => {
     const response = new Response("", {
       status: 400,
       headers: { "Content-Type": "application/json" },
@@ -61,6 +61,21 @@ describe("handleError", () => {
     );
     await expect(handleError(response)).rejects.toThrow(
       new PachcaClientError("Клиентская ошибка", response, errors),
+    );
+  });
+
+  it("should throw a PachcaError if the response is XML", async () => {
+    const response = new Response(
+      `<?xml version="1.0" encoding="UTF-8"?>
+    <Error><Code>AccessDenied</Code><Message>Access Denied</Message><Resource>/</Resource><RequestId>06c0ba73-20cf-48fa-8616-607f79884981</RequestId></Error>`,
+      {
+        status: 403,
+        headers: { "Content-Type": "text/xml" },
+      },
+    );
+
+    await expect(handleError(response)).rejects.toThrow(
+      new PachcaError("AccessDenied Access Denied", response),
     );
   });
 
--- src/api-client/handle-error.ts
+++ src/api-client/handle-error.ts
@@ -4,15 +4,27 @@ export async function handleError(response: Response): Promise<never> {
   if (!response.headers.has("Content-Type"))
     throw new PachcaError("В ответе нет заголовка Content-Type", response);
 
-  if (!response.headers.get("Content-Type")?.includes("application/json"))
+  const contentType = response.headers.get("Content-Type");
+
+  if (
+    !contentType?.includes("application/json") &&
+    !contentType?.includes("text/xml")
+  )
     throw new PachcaError(
-      "Заголовок Content-Type в ответе не application/json",
+      `Заголовок Content-Type ${contentType} в ответе не поддерживается`,
       response,
     );
 
   const text = await response.text();
 
   if (!text) throw new PachcaError("Тело ответа пустое", response);
+
+  if (contentType?.includes("text/xml")) {
+    const code = /<Code>(.+?)<\/Code>/.exec(text)?.at(1);
+    const message = /<Message>(.+?)<\/Message>/.exec(text)?.at(1);
+
+    throw new PachcaError(`${code} ${message}`, response);
+  }
 
   const data: unknown = JSON.parse(text);
 
